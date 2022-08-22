7b3012b09b50da3214a37ec861e35acb9980270b
ginghisken <finstreschwarze@gmail.com>
2022-08-22 14:53:23 -0400

    Fixed materia animation to work like a bow, added rudimentary function of placing NBT tag on offhand item.
    Signed-off-by: ginghisken <finstreschwarze@gmail.com>

--- src/main/java/com/dndads/artifice/items/Materia.java
+++ src/main/java/com/dndads/artifice/items/Materia.java
@@ -11,9 +11,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.UseAction;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 
 import javax.swing.*;
@@ -42,23 +44,64 @@ public class Materia extends Item {
     // the actual effect of using the item.
     @Override
     public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand hand) {
+
         ItemStack mainHandItem = playerIn.getMainHandItem();
         ItemStack offhandItem = playerIn.getOffhandItem();
 
-        // get offhand item's attack damage in the form of a string.
-        AttributeModifier testAttrib = (AttributeModifier) offhandItem.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).toArray()[0];
+        // get the player's offhand item's NBT tags.
+        CompoundNBT offhandItemNbt = offhandItem.getTag();
 
-        System.out.println(mainHandItem);
-        System.out.println(offhandItem);
-        System.out.println("Offhand Item Base Damage: " + testAttrib.getAmount());
+        // If the offhand item doesn't have null tags and also doesn't have the 'melded' tag, add the melded tag with a value of true.
+        if (offhandItemNbt != null && offhandItemNbt.contains("melded") == false) {
+            offhandItemNbt.putBoolean("melded", true);
+        }
 
+
+        //Run the actual use animation.
+        playerIn.startUsingItem(hand);
+
+        //Complete the use successfully.
         return new ActionResult<ItemStack>(ActionResultType.SUCCESS, mainHandItem);
     }
 
+/*
+
+        // get offhand item's NBT tags.
+        CompoundNBT offhandItemNbt = offhandItem.getTag();
+
+        if (offhandItemNbt != null && offhandItemNbt.contains("melded") == false) {
+            offhandItemNbt.putBoolean("melded", true);
+        }
+
+        /*
+
+        else {
+            if (worldIn.isClientSide == false) {
+                playerIn.sendMessage(new StringTextComponent("Weapon is already melded!"),
+                        null);
+            }
+        }
+
+         */
+        /*
+
+        // get offhand item's attack damage in the form of a string (CURRENTLY BROKEN, breaks because it would fire on stuff like air).
+        // AttributeModifier testAttrib = (AttributeModifier) offhandItem.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).toArray()[0];
+
+        System.out.println(mainHandItem);
+        System.out.println(offhandItem);
+        // System.out.println("Offhand Item Base Damage: " + testAttrib.getAmount());
+
+        return new ActionResult<ItemStack>(ActionResultType.SUCCESS, mainHandItem);
+    }
+    */
+
     // returns an empty itemstack at the end of use.
     // add conditionals here for not deleting if there is no valid item to apply a modifier to?
+
     @Override
     public ItemStack finishUsingItem(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+
         return ItemStack.EMPTY;
     }
 

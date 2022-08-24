42f0f1965348860adc8e22796c7c319478254fe9
ginghisken <finstreschwarze@gmail.com>
2022-08-23 21:30:26 -0400

    Materia bugs galore.
    Signed-off-by: ginghisken <finstreschwarze@gmail.com>

--- src/main/java/com/dndads/artifice/items/Materia.java
+++ src/main/java/com/dndads/artifice/items/Materia.java
@@ -51,7 +51,6 @@ public class Materia extends Item {
 
         ItemStack mainHandItem = playerIn.getMainHandItem();
 
-
         // Run the actual use animation.
         playerIn.startUsingItem(hand);
 
@@ -72,14 +71,34 @@ public class Materia extends Item {
         // Get the player's offhand item's NBT tags.
         CompoundNBT offhandItemNbt = offhandItem.getTag();
 
+        //Check the name of the offhand item and output as a string.
+        ItemStack offhandItemStack = entityLiving.getOffhandItem();
+        Item offhandItemTest = offhandItemStack.getItem();
+        String offhandItemName = offhandItemTest.toString();
+
         // If the offhand item doesn't have null tags and also doesn't have the 'melded' tag, add the melded tag with a value of true.
-        if (offhandItemNbt != null && offhandItemNbt.contains("melded") == false) {
+        // Also checks if the offhand item contains the ATTACK_DAMAGE attribute in the first place.
+        // ALSO checks that you don't have materia in your offhand.
+        if (offhandItemNbt != null && offhandItemNbt.contains("melded") == false
+                && offhandItem.getAttributeModifiers(EquipmentSlotType.MAINHAND).containsKey(Attributes.ATTACK_DAMAGE)
+                && offhandItemName != "materia") {
             offhandItemNbt.putBoolean("melded", true);
 
             // Play a success sound.
             if (worldIn.isClientSide) {
                 worldIn.playLocalSound(xPos, yPos, zPos, SoundEvents.ANVIL_PLACE, SoundCategory.PLAYERS, 1.0F, 1.0F, false);
             }
+
+            // Get and record the offhand weapon's base damage.
+            AttributeModifier testAttrib = (AttributeModifier) offhandItem.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).toArray()[0];
+            double baseDamageAmount = testAttrib.getAmount();
+            System.out.println("base: " + baseDamageAmount);
+
+            // Add 5 to the offhand weapon's base damage.
+            double newDamageAmount = baseDamageAmount + 5;
+
+            // Add a new modifier that overwrites the old damage with the new higher value.
+            offhandItem.addAttributeModifier(Attributes.ATTACK_DAMAGE, new AttributeModifier("extraDamage", newDamageAmount, AttributeModifier.Operation.ADDITION), EquipmentSlotType.MAINHAND);
 
             // Get the player's main hand and move the offhand item into it.
             // >>>>>(THIS DOES NOT WORK BUT IT WAS A NICE TRY, revisit later)<<<<<
